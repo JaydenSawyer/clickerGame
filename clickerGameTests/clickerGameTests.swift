fe87aa2dc379e55a4545599f115b7cfef64c0ce5
Jayden Sawyer <jayden.sawyer25@d155.org>
2024-09-25 07:28:40 -0500

    Initial Commit

---
+++ clickerGameTests/clickerGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  clickerGameTests.swift
+//  clickerGameTests
+//
+//  Created by JAYDEN SAWYER on 9/25/24.
+//
+
+import Testing
+@testable import clickerGame
+
+struct clickerGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

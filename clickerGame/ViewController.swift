309090cc7459f4038191dd93c94d9bbb39a07420
Jayden Sawyer <jayden.sawyer25@d155.org>
2024-09-26 08:05:01 -0500

    1

--- clickerGame/ViewController.swift
+++ clickerGame/ViewController.swift
@@ -4,11 +4,15 @@
 //
 //  Created by JAYDEN SAWYER on 9/25/24.
 //
+class points{
+    static var points = 0
+}
 
 import UIKit
 
 class ViewController: UIViewController {
-
+   var count = 0
+    @IBOutlet weak var coinLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,7 +20,8 @@ class ViewController: UIViewController {
 
 
     @IBAction func clickAction(_ sender: UIButton) {
-        
+        count += 1
+        coinLabel.text = "Coins: \(count)"
     }
 }
 
---
+++ clickerGame/ViewController2.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController2.swift
+//  clickerGame
+//
+//  Created by JAYDEN SAWYER on 9/26/24.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    @IBOutlet weak var error2Label: UILabel!
+    
+    @IBOutlet weak var error1Laebl: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+    @IBAction func upgrade1Button(_ sender: UIButton) {
+        
+            }
+    
+    @IBAction func upgrade2Button(_ sender: UIButton) {
+        
+    }
+    
+    
+
+}

424637b199aadfb1831967af734a1ed30e7cd8e5
Jayden Sawyer <jayden.sawyer25@d155.org>
2024-10-23 08:08:40 -0500

    yoooo

--- clickerGame/ViewController3.swift
+++ clickerGame/ViewController3.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController3: UIViewController {
+class ViewController3: UIViewController, UITableViewDelegate, UITableViewDataSource {
+        
+    var games = ["Game: 1", "Game: 2", "Game: 3", "Game: 4", "Game: 5"]
 
+
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return games.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = games[indexPath.row]
+               return cell
+
+    }
 
 }
